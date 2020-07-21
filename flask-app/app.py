10ecf9c51084186e3417652ea49de57171316f67
Olivier Cervello <ocervello@google.com>
2020-07-21 21:53:58 +0200

    Add working configuration

--- flask-app/app.py
+++ flask-app/app.py
@@ -9,14 +9,13 @@ from opentelemetry.ext.requests import RequestsInstrumentor
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
 from opentelemetry.sdk.metrics import Counter, MeterProvider
-from opentelemetry.sdk.metrics.export.controller import PushController
 
 # Method 1: Export to OT collector
 from opentelemetry.ext.opencensusexporter.metrics_exporter import OpenCensusMetricsExporter
 from opentelemetry.ext.opencensusexporter.trace_exporter import OpenCensusSpanExporter
-span_exporter = OpenCensusSpanExporter(service_name="flask-app", endpoint="localhost:55678")
-exporter = OpenCensusMetricsExporter(
-    service_name="flask-app", endpoint="localhost:55678")
+OTEL_AGENT_ENDPOINT = os.environ['OTEL_AGENT_ENDPOINT']
+span_exporter = OpenCensusSpanExporter(service_name="flask-app-tutorial", endpoint=OTEL_AGENT_ENDPOINT)
+exporter = OpenCensusMetricsExporter(service_name="flask-app-tutorial", endpoint=OTEL_AGENT_ENDPOINT)
 
 # Method 2: Export to Cloud Ops (WON'T WORK WITH GUNICORN)
 #from opentelemetry.exporter.cloud_monitoring import CloudMonitoringMetricsExporter
@@ -28,8 +27,7 @@ exporter = OpenCensusMetricsExporter(
 # Metrics
 metrics.set_meter_provider(MeterProvider())
 meter = metrics.get_meter(__name__, True)
-controller = PushController(meter, exporter, 5)
-
+metrics.get_meter_provider().start_pipeline(meter, exporter, 5)
 
 # Traces
 trace.set_tracer_provider(TracerProvider())
@@ -37,15 +35,14 @@ trace.get_tracer_provider().add_span_processor(
     BatchExportSpanProcessor(span_exporter))
 
 # Custom metrics
-pid = str(os.getpid())
-staging_labels = {"environment": "staging", "pid": pid}
+staging_labels = {"environment": "staging"}
 requests_counter = meter.create_metric(
     name="testcounter",
-    description="Number of orders",
+    description="A test counter (custom metrics)",
     unit="1",
     value_type=int,
     metric_type=Counter,
-    label_keys=("environment", "pid",),
+    label_keys=("environment",),
 )
 
 # Flask application
@@ -53,16 +50,18 @@ app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
+# Logging setup
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
 
 @app.route("/")
 def hello():
     app.logger.info("Received hello request !")
     requests_counter.add(1, staging_labels)
+    app.logger.debug("Counter was incremented.")
     requests.get('https://www.google.com')
     return "Hello World!"
 
 if __name__ == "__main__":
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
