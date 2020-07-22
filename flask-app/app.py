540692f7f09499fc51448702bad87b570dc5430a
Olivier Cervello <ocervello@google.com>
2020-07-22 11:51:42 +0200

    Agentless setup (no OT agent / collector needed

--- flask-app/app.py
+++ flask-app/app.py
@@ -11,56 +11,53 @@ from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
 from opentelemetry.sdk.metrics import Counter, MeterProvider
 
 # Method 1: Export to OT collector
-from opentelemetry.ext.opencensusexporter.metrics_exporter import OpenCensusMetricsExporter
-from opentelemetry.ext.opencensusexporter.trace_exporter import OpenCensusSpanExporter
-OTEL_AGENT_ENDPOINT = os.environ['OTEL_AGENT_ENDPOINT']
+#from opentelemetry.ext.opencensusexporter.metrics_exporter import OpenCensusMetricsExporter
+#from opentelemetry.ext.opencensusexporter.trace_exporter import OpenCensusSpanExporter
+#OTEL_AGENT_ENDPOINT = os.environ['OTEL_AGENT_ENDPOINT']
 #span_exporter = OpenCensusSpanExporter(service_name="flask-app-tutorial", endpoint=OTEL_AGENT_ENDPOINT)
-exporter = OpenCensusMetricsExporter(service_name="flask-app-tutorial", endpoint=OTEL_AGENT_ENDPOINT)
+#exporter = OpenCensusMetricsExporter(service_name="flask-app-tutorial", endpoint=OTEL_AGENT_ENDPOINT)
 
-# Method 2: Export to Cloud Ops (WON'T WORK WITH GUNICORN)
-#from opentelemetry.exporter.cloud_monitoring import CloudMonitoringMetricsExporter
-#from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
-#span_exporter = CloudTraceSpanExporter()
-#exporter = CloudMonitoringMetricsExporter()
-
+# Method 2: Export to Cloud Ops directly
+from opentelemetry.exporter.cloud_monitoring import CloudMonitoringMetricsExporter
+from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+span_exporter = CloudTraceSpanExporter()
+exporter = CloudMonitoringMetricsExporter(add_unique_identifier=True)
 
 # Metrics
 metrics.set_meter_provider(MeterProvider())
 meter = metrics.get_meter(__name__, True)
-metrics.get_meter_provider().start_pipeline(meter, exporter, 5)
+metrics.get_meter_provider().start_pipeline(meter, exporter, interval=1)
 
 # Traces
-#trace.set_tracer_provider(TracerProvider())
-#trace.get_tracer_provider().add_span_processor(
-#    BatchExportSpanProcessor(span_exporter))
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    BatchExportSpanProcessor(span_exporter))
 
 # Custom metrics
-pid = os.getpid()
-staging_labels = {"environment": "staging", "pid": pid}
-requests_counter = meter.create_metric(
-    name="testcounter",
-    description="A test counter (custom metrics)",
+staging_labels = {"environment": "staging", "app": "flask-app-tutorial"}
+hello_requests_counter = meter.create_metric(
+    name="hello_requests",
+    description="A test counter for hello requests",
     unit="1",
     value_type=int,
     metric_type=Counter,
-    label_keys=("environment", "pid",),
+    label_keys=("environment", "pid", "app",),
 )
 
 # Flask application
 app = Flask(__name__)
-#FlaskInstrumentor().instrument_app(app)
-#RequestsInstrumentor().instrument()
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 
 # Logging setup
 gunicorn_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers = gunicorn_logger.handlers
 app.logger.setLevel(gunicorn_logger.level)
+#app.logger.info(f'OpenTelemetry endpoint: {OTEL_AGENT_ENDPOINT}')
 
 @app.route("/")
 def hello():
-    app.logger.info("Received hello request !")
-    requests_counter.add(1, staging_labels)
-    app.logger.debug("Counter was incremented.")
+    hello_requests_counter.add(1, staging_labels)
     requests.get('https://www.google.com')
     return "Hello World!"
 
